51889d8d952b4ee4450ccaa31c40a20449856899
carlosmaya326 <carlosmaya326@gmail.com>
2020-08-29 21:26:07 -0500

    Solo un archivo

---
+++ view/vPrueba.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+
+<body>
+	<h4 class="title">Me gusta la carne la leche y el pan</h4>
+</body>
+
+</html>
